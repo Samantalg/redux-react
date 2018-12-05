ba5746494e77376a4b7f529415923d39dbffb8e6
Samanta <slaragiannone@gmail.com>
2018-12-05 12:24:07 +0000

    exercise done

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Modulo 9 EOI
+
+Ejercicio hecho en el curso de FullStack de la EOI TFN 2018
---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import * as Redux from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Header from './containers/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Movies from './pages/Movies';
+import rootReducer, { AppState } from './reducers';
+import { decrementCounter, incrementCounter } from './actions';
+
+const store: Redux.Store<AppState> = Redux.createStore(rootReducer);
+
+function increment() {
+  store.dispatch(incrementCounter(1));
+}
+
+function decrement() {
+  store.dispatch(decrementCounter(1));
+}
+
+export default class App extends React.Component {
+  render() {
+    return <Provider store={store}>
+      <Router>
+        <div>
+          <Header/>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <Route exact path="/" component={Home}/>
+          <Route path="/home" component={Home}/>
+        </div>
+      </Router>
+    </Provider>;
+  }
+}
